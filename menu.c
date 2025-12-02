8d6f1c175413c9d9918a9c8d6de2e5dd6de98bc4
Dhriti Dhriti <107620606+dhriti29124@users.noreply.github.com>
2025-12-02 17:37:11 -0400

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "fastfood.h"
 
-void addMenuItem() {}
+void addMenuItem() {
+  MenuItem item;
+  FILE *fp = fopen ("menu.txt","a"); 
+
+  if (fp== NULL){
+    printf("Error opening the menu file!\n");
+    return;
+  }
+  printf("Enter Item ID: ");
+    scanf("%d", &item.itemID);
+
+    printf("Enter Item Name: ");
+    scanf("%s", item.name);
+
+    printf("Enter Category: ");
+    scanf("%s", item.category);
+
+    printf("Enter Price: ");
+    scanf("%f", &item.price);
+
+    printf("Is Available? (1 = Yes, 0 = No): ");
+    scanf("%d", &item.isAvailable);
+
+  fprintf(fp, %d %s %s %.2f %d\n"
+         item.itemID, item.name, item.category, item.price, item.isAvailable);
+  fclose(fp);
+
+  printf("Menu item added successfully!\n");
+}
+  
+}
 void listMenuItems() {}
 void searchMenuItemByID(int searchID) {}
