a423dcdd0e0a7d372b7d6b949662e9c103d7bca9
Dhriti Dhriti <107620606+dhriti29124@users.noreply.github.com>
2025-12-02 17:40:11 -0400

    Update menu.c

--- menu.c
+++ menu.c
@@ -34,5 +34,67 @@ void addMenuItem() {
 }
   
 }
-void listMenuItems() {}
-void searchMenuItemByID(int searchID) {}
+void listMenuItems() {
+  
+    MenuItem item;
+    FILE *fp = fopen("menu.txt", "r");
+
+    if (fp == NULL) {
+        printf("Menu file not found! Please add items first.\n");
+        return;
+    }
+
+    printf("\n----- MENU ITEMS -----\n");
+
+    while (fscanf(fp, "%d %s %s %f %d",
+                  &item.itemID, item.name, item.category, &item.price, &item.isAvailable) == 5) {
+
+        printf("ID: %d | Name: %s | Category: %s | Price: %.2f | Available: %s\n",
+               item.itemID,
+               item.name,
+               item.category,
+               item.price,
+               item.isAvailable ? "Yes" : "No");
+    }
+
+    fclose(fp);
+}
+
+
+void searchMenuItemByID() {
+    int id;
+    int found = 0;
+    MenuItem item;
+
+    printf("Enter Item ID to search: ");
+    scanf("%d", &id);
+
+    FILE *fp = fopen("menu.txt", "r");
+
+    if (fp == NULL) {
+        printf("Menu file not found!\n");
+        return;
+    }
+
+    while (fscanf(fp, "%d %s %s %f %d",
+                  &item.itemID, item.name, item.category, &item.price, &item.isAvailable) == 5) {
+
+        if (item.itemID == id) {
+            printf("\n----- ITEM FOUND -----\n");
+            printf("ID: %d\n", item.itemID);
+            printf("Name: %s\n", item.name);
+            printf("Category: %s\n", item.category);
+            printf("Price: %.2f\n", item.price);
+            printf("Available: %s\n", item.isAvailable ? "Yes" : "No");
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Item with ID %d NOT FOUND in the menu.\n", id);
+    }
+
+    fclose(fp);
+}
+
