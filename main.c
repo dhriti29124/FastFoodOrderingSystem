4adbabb45d064b5ef573caa4c8e86bba69887c0d
Dhriti Dhriti <107620606+dhriti29124@users.noreply.github.com>
2025-12-02 17:51:39 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include "fastfood.h"
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\n\n===== FAST FOOD ORDERING SYSTEM =====\n");
+        printf("1. Add New Menu Item\n");
+        printf("2. List All Menu Items\n");
+        printf("3. Search Menu Item by ID\n");
+        printf("0. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+
+            case 1:
+                addMenuItem();
+                break;
+
+            case 2:
+                listMenuItems();
+                break;
+
+            case 3:
+                searchMenuItemByID();
+                break;
+
+            case 0:
+                printf("Exiting program... Goodbye!\n");
+                return 0;
+
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
+
+    return 0;
+}
 
