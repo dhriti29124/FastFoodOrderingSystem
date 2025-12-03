ac69838d3901c20452713761bfa5e72333a9ed80
ehs7aan <145816168+ehs7aan@users.noreply.github.com>
2025-12-03 12:44:41 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 int main() {
     int choice;
 
+    //main program loop - runs until user chooses to exit
     while (1) {
+        //displays our main menu options
         printf("\n\n===== FAST FOOD ORDERING SYSTEM =====\n");
         printf("1. Add New Menu Item\n");
         printf("2. List All Menu Items\n");
@@ -14,40 +16,50 @@ int main() {
         printf("6. Generate Sales Report\n");
         printf("0. Exit\n");
 
+        //get user chioce
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
+        //switch statements
         switch (choice) {
 
             case 1:
+                //adds a new item to mneu
                 addMenuItem();
                 break;
 
             case 2:
+                //display all menu items only available onces
                 listMenuItems();
                 break;
 
             case 3:
+                //searches for specific menu item by id no.
                 searchMenuItemByID();
                 break;
 
             case 4:
+                //process customer order
                 placeOrder();
                 break;
 
             case 5:
+                //modify inventory quantities by items 
                 updateInventory();
                 break;
 
             case 6:
+                //displays sales report
                 generateSalesReport();
                 break;
 
             case 0:
+                //exit 
                 printf("Exiting program... Goodbye!\n");
                 return 0;
 
             default:
+                //error handling or invalid menu selection
                 printf("Invalid choice! Please try again.\n");
         }
     }
