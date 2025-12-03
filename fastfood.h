6a35cd67e3504b732ef9c34ef28e292044006a62
ehs7aan <145816168+ehs7aan@users.noreply.github.com>
2025-12-02 22:08:21 -0400

    Add menu and order file definitions in fastfood.h

--- fastfood.h
+++ fastfood.h
@@ -1,12 +1,15 @@
 #ifndef FASTFOOD_H
 #define FASTFOOD_H
 
+#define MENU_FILE "menu.txt"
+#define ORDER_FILE "orders.txt"
+
 typedef struct {
     int itemID;
     char name[50];
     char category[20];
     float price;
-    int isAvailable; 
+    int isAvailable;
 } MenuItem;
 
 typedef struct {
@@ -20,9 +23,10 @@ void addMenuItem();
 void listMenuItems();
 void searchMenuItemByID();
 
-// not my part 
+
 void placeOrder();
 void updateInventory();
 void generateSalesReport();
 
 #endif
+
