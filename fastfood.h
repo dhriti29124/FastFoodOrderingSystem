327cc0be48b21e25450fd9af7dc6bd96b032f372
ehs7aan <145816168+ehs7aan@users.noreply.github.com>
2025-12-03 12:52:11 -0400

    Update fastfood.h

--- fastfood.h
+++ fastfood.h
@@ -1,29 +1,34 @@
 #ifndef FASTFOOD_H
 #define FASTFOOD_H
 
-#define MENU_FILE "menu.txt"
+//stores menu item info
+#define MENU_FILE "menu.txt" 
+//stores order 
 #define ORDER_FILE "orders.txt"
 
+//menu item structure 
 typedef struct {
-    int itemID;
-    char name[50];
-    char category[20];
-    float price;
-    int isAvailable;
+    int itemID; //menu id
+    char name[50]; //name of item 
+    char category[20]; //eg. burger, drinks , dessert
+    float price; //price of item in  $
+    int isAvailable; //check availability
 } MenuItem;
 
+//order stucture
 typedef struct {
-    int orderID;
-    int itemID;
-    int quantity;
-    float totalCost;
+    int orderID; //order id 
+    int itemID; //menu id of the order 
+    int quantity; //no of item ordered 
+    float totalCost; //price * quantity = total
 } Order;
 
+//menu functions
 void addMenuItem();
 void listMenuItems();
 void searchMenuItemByID();
 
-
+//order and inventory functions
 void placeOrder();
 void updateInventory();
 void generateSalesReport();
